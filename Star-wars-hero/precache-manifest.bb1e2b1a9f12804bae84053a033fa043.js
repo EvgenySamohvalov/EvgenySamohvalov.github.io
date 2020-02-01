caf065e25b3ca581b4648e1c022b64c607edfa35
unknown <evgenysamohvalov1988@gmail.com>
2020-02-01 18:27:40 +0300

    init: add progect Star-wars-hero

---
+++ Star-wars-hero/precache-manifest.bb1e2b1a9f12804bae84053a033fa043.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f209f459a0158030115041ce18f785c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51819ecd96d771de0fb4",
+    "url": "/static/css/main.c0b5f6f5.chunk.css"
+  },
+  {
+    "revision": "87024a022cc411bbd9ca",
+    "url": "/static/js/2.9b1666ac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9b1666ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51819ecd96d771de0fb4",
+    "url": "/static/js/main.5d34963d.chunk.js"
+  },
+  {
+    "revision": "c30697aeda529e0a099a",
+    "url": "/static/js/runtime-main.29c7c2bd.js"
+  },
+  {
+    "revision": "8f4b4f89f7b9389906039ca8f0752854",
+    "url": "/static/media/BeruWhitesunlars.8f4b4f89.jpg"
+  },
+  {
+    "revision": "daac3de8db19ac57fada2bcd31f13903",
+    "url": "/static/media/BiggsDarklighter.daac3de8.jpg"
+  },
+  {
+    "revision": "ce3e60472412c87c1830f521a5ea25b9",
+    "url": "/static/media/C-3PO.ce3e6047.jpg"
+  },
+  {
+    "revision": "0f26e9152f692ca68d3559a4c8fb5407",
+    "url": "/static/media/DarthVader.0f26e915.jpg"
+  },
+  {
+    "revision": "53c0b037b0f025b49c1991033a2f6ff9",
+    "url": "/static/media/LeiaOrgana.53c0b037.jpg"
+  },
+  {
+    "revision": "c5857f7d16a6c064bf27143efa18c87b",
+    "url": "/static/media/LukeSkywalker.c5857f7d.jpg"
+  },
+  {
+    "revision": "74aeb9f85778237508ee914b26018e88",
+    "url": "/static/media/Obi-WanKenobi.74aeb9f8.jpg"
+  },
+  {
+    "revision": "dac799747cc03339b1a85b0887b7d05c",
+    "url": "/static/media/OwenLars.dac79974.jpg"
+  },
+  {
+    "revision": "79c869eef60997646c59a0043bedaa22",
+    "url": "/static/media/R2-D2.79c869ee.jpg"
+  },
+  {
+    "revision": "210c16d20a0f65f2050093c10f209b7e",
+    "url": "/static/media/R5-D4.210c16d2.jpg"
+  },
+  {
+    "revision": "32fe4dc6ed8b840c320a6934142eff9f",
+    "url": "/static/media/loader.32fe4dc6.svg"
+  }
+]);
